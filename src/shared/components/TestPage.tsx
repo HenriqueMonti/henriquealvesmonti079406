c72ee20c4151cd76339f4f8bbfa8c104244deb9d
Henrique Alves Monti <114963779+HenriqueMonti@users.noreply.github.com>
2026-02-03 11:33:39 -0400

    chore: configuração do Tailwind e estilos globais
    Configuração do Tailwind CSS para estilização do projeto, incluindo estilos globais e base para layout responsivo.

---
+++ src/shared/components/TestPage.tsx
@@ -0,0 +1,9 @@
+export const TestPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-300 text-gray-800">
+      <h1 className="text-2xl font-semibold">
+        App Base
+      </h1>
+    </div>
+  );
+};
